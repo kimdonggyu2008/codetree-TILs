1712cc85d5afef7391d0769f113cab5a053e3765
DongGyu Kim <57940897+kimdonggyu2008@users.noreply.github.com>
2024-05-03 18:16:28 +0900

    Title: 금 채굴하기, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240503/금 채굴하기/gold-mining.py
+++ 240503/금 채굴하기/gold-mining.py
@@ -3,11 +3,11 @@ a = [list(map(int, input().split())) for _ in range(n)]
 
 best=0
 
-def biggest(k,x,y):
+def biggest(x,y,k):#갯수 세기
     total=0
     for i in range(n):
         for j in range(n):
-            if abs(x-i)+abs(y-j)<=k:
+            if abs(x-i)+abs(y-j)<=k:#마름모 중간 기준 위치 다 보기
                 total+=a[i][j]
     return total
 
@@ -18,7 +18,7 @@ def digprice(k):
 for i in range(n):
     for j in range(n):
         for k in range(n):
-            if(biggest(k,i,j)*m>digprice(k)and best<biggest(k,i,j)):
-                best=biggest(k,i,j)
+            if(biggest(i,j,k)*m>digprice(k) and best<biggest(i,j,k)):
+                best=biggest(i,j,k)
 
 print(best)
