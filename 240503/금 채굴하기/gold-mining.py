55574adc3a1bc2b6190c2c9da370f6d69e8281b2
DongGyu Kim <57940897+kimdonggyu2008@users.noreply.github.com>
2024-05-03 18:37:09 +0900

    Title: 금 채굴하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240503/금 채굴하기/gold-mining.py
+++ 240503/금 채굴하기/gold-mining.py
@@ -1,4 +1,3 @@
-"""
 n, m = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
 
@@ -10,7 +9,7 @@ def biggest(x,y,k):#갯수 세기
         for j in range(n):
             if abs(x-i)+abs(y-j)<=k:#마름모 중간 기준 위치 다 보기
                 total+=a[i][j]
-        print(total)
+
     return total
 
 def digprice(k):
@@ -23,36 +22,6 @@ for i in range(n):
             now=biggest(i,j,k)
             if((now*m)>=digprice(k) and best<now):
                 best=now
-
+if(not(0 in a)):
+    best=n*n
 print(best)
-                
-"""
-n , goldPrice = list(map(int,input().split()))
-
-
-goldCave = [
-    list(map(int,input().split()))
-    for _ in range(n)
-]
-
-bestHigh = 0
-
-def howManyGold( x , y , k ):
-    totalGold = 0
-    for i in range(n):
-        for j in range(n):
-            if abs(x - i) + abs(y - j) <= k:
-                totalGold += goldCave[i][j]
-    return totalGold
-
-def digPrice (k):
-    return k * k + ( k + 1 ) * ( k + 1 )
-
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            if (howManyGold( i , j , k ) * goldPrice) >= digPrice(k) and bestHigh < howManyGold( i , j , k ):
-                bestHigh = howManyGold( i , j , k )
-                
-
-print(bestHigh)
