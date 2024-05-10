7a908d6ac019c41b930ba7f5f96e636c6abc605e
DongGyu Kim <57940897+kimdonggyu2008@users.noreply.github.com>
2024-05-10 23:26:27 +0900

    Title: 양수 직사각형의 최대 크기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240510/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240510/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,3 +1,4 @@
+"""
 import sys
 INT_MIN=-sys.maxsize
 
@@ -16,7 +17,6 @@ def find_max(n,m):
                     if(temp>max_sum):
                         max_sum=temp
                         max_size=(l-i+1)*(k-j+1)
-
     return max_size
 
 
@@ -25,4 +25,38 @@ if __name__=="__main__":
     n,m=map(int,input().split())
     a=[list(map(int,input().split()))for _ in range(n)]
     result=find_max(n,m)
+    print(result)
+"""
+
+import sys
+
+INT_MIN = -sys.maxsize
+
+
+def find_max_sum(matrix):
+    n, m = len(matrix), len(matrix[0])
+    msisdp = [[0 for _ in range(m)] for _ in range(n)]  # Maximum Sum in Single Subarray (DP table)
+    max_size = 0
+    max_so_far = INT_MIN
+
+    for i in range(n):
+        for j in range(m):
+            msisdp[i][j] = matrix[i][j]
+
+            if i > 0 and msisdp[i - 1][j] > 0:
+                msisdp[i][j] += msisdp[i - 1][j]
+
+    for i in range(n):
+        for j in range(m):
+            if msisdp[i][j] > max_so_far:
+                max_so_far = msisdp[i][j]
+                max_size = (i + 1) * (j + 1)
+
+    return max_size
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    matrix = [[int(x) for x in input().split()] for _ in range(n)]
+    result = find_max_sum(matrix)
     print(result)
