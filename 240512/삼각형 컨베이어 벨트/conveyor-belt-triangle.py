e6ccdf4f193d6e14c797bfe829f4d0a6ec1aa617
DongGyu Kim <57940897+kimdonggyu2008@users.noreply.github.com>
2024-05-12 23:19:21 +0900

    Title: 삼각형 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240512/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,16 @@
+n,t=map(int,input().split())
+a=[list(map(int,input().split()))for _ in range(n)]
+for k in range(t):
+    last3=a[2][-1]
+    last2=a[1][-1]
+    last1=a[0][-1]
+    for i in range(3):
+        for j in range(n-1,0,-1):
+            a[i][j]=a[i][j-1]
+    a[2][0]=last2
+    a[1][0]=last1
+    a[0][0]=last3
+for row in a:
+    for column in row:
+        print(column,end=' ')
+    print()
