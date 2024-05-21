e246a11b8f34c26e209c4cbfcf77fe92ced42172
DongGyu Kim <57940897+kimdonggyu2008@users.noreply.github.com>
2024-05-21 23:10:49 +0900

    Title: 십자 모양 폭발, Time: 166ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240521/십자 모양 폭발/cross-shape-bomb.py
+++ 240521/십자 모양 폭발/cross-shape-bomb.py
@@ -1,4 +1,3 @@
-"""
 def boom(a,x,y,n):
     length=a[x-1][y-1]-1
     #print(x,y,length)
@@ -14,16 +13,18 @@ def boom(a,x,y,n):
         a[x-1][j]=0
     #a[left:right][y]=0
     return a
-
-def drop(a,n):
-    for i in range(n,0):
-        for j in range(n,0):
-            if(a[i][j]==0 and i!=0):
-                print(i,j)
-                a[i][j]=a[i-1][j]
-                a[i-1][j]=0
+def drop(a, n):
+    # Iterate from bottom to top and right to left to handle dropping elements
+    for j in range(n):
+        for i in range(n-1, 0, -1):
+            if a[i][j] == 0:
+                # Move elements down
+                k = i
+                while k > 0 and a[k][j] == 0:
+                    a[k][j] = a[k-1][j]
+                    a[k-1][j] = 0
+                    k -= 1
     return a
-
 n=int(input())
 a=[]
 for i in range(n):
@@ -38,41 +39,3 @@ for i in range(n):
     for j in range(n):
         print(a[i][j],end=' ')
     print('')
-"""
-
-# 변수 선언 및 입력
-n = int(input())
-numbers = [
-    int(input())
-    for _ in range(n)
-]
-end_of_array = n
-
-
-# 입력 배열에서 지우고자 하는 부분 수열을 삭제합니다.
-def cut_array(start_idx, end_idx):
-    global end_of_array, numbers
-    
-    temp_arr = []
-    
-    # 구간 외의 부분만 temp 배열에 순서대로 저장합니다.
-    for i in range(end_of_array):
-        if i < start_idx or i > end_idx:
-            temp_arr.append(numbers[i])
-
-    # temp 배열을 다시 numbers 배열로 옮겨줍니다.
-    end_of_array = len(temp_arr)
-    for i in range(end_of_array):    
-        numbers[i] = temp_arr[i]
-
-
-# 두 번에 걸쳐 지우는 과정을 반복합니다.
-for _ in range(2):
-    s, e = tuple(map(int, input().split()))
-    # [s, e] 구간을 삭제합니다.
-    cut_array(s - 1, e - 1)
-
-# 출력:
-print(end_of_array)
-for i in range(end_of_array):
-    print(numbers[i])
